5696d2497909edef8d96ec537c95d808c53ef10d
Fayaz Ahmed <fayazmiraz@users.noreply.github.com>
2025-01-20 09:47:05 +0600

    Update start, stop, pause

--- newtab.js
+++ newtab.js
@@ -1,31 +1,62 @@
 let timer;
 let isRunning = false;
+let isPaused = false;
 let timeLeft = 10; // 60 seconds countdown
 
 function updateTimerDisplay() {
   document.getElementById('timer').textContent = timeLeft + 's';
 }
 
+function startTimer() {
+  timer = setInterval(() => {
+    if (timeLeft > 0) {
+      timeLeft--;
+      updateTimerDisplay();
+    } else {
+      clearInterval(timer);
+      isRunning = false;
+      document.getElementById('startButton').style.display = 'inline-block';
+      document.getElementById('pauseButton').style.display = 'none';
+      document.getElementById('stopButton').style.display = 'none';
+      timeLeft = 10;
+      updateTimerDisplay();
+    }
+  }, 1000);
+}
+
 function toggleTimer() {
   if (isRunning) {
     clearInterval(timer);
+    isRunning = false;
+    isPaused = true;
+    document.getElementById('pauseButton').textContent = 'Resume';
   } else {
-    timer = setInterval(() => {
-      if (timeLeft > 0) {
-        timeLeft--;
-        updateTimerDisplay();
-      } else {
-        clearInterval(timer);
-        isRunning = false;
-        document.getElementById('toggleButton').textContent = 'Start';
-        timeLeft = 10;
-        updateTimerDisplay();
-      }
-    }, 1000);
+    if (isPaused) {
+      startTimer();
+      isPaused = false;
+      document.getElementById('pauseButton').textContent = 'Pause';
+    } else {
+      startTimer();
+    }
+    isRunning = true;
   }
-  isRunning = !isRunning;
-  document.getElementById('toggleButton').textContent = isRunning ? 'Stop' : 'Start';
+  document.getElementById('startButton').style.display = 'none';
+  document.getElementById('pauseButton').style.display = 'inline-block';
+  document.getElementById('stopButton').style.display = 'inline-block';
 }
 
-document.getElementById('toggleButton').addEventListener('click', toggleTimer);
+function stopTimer() {
+  clearInterval(timer);
+  isRunning = false;
+  isPaused = false;
+  timeLeft = 10;
+  updateTimerDisplay();
+  document.getElementById('startButton').style.display = 'inline-block';
+  document.getElementById('pauseButton').style.display = 'none';
+  document.getElementById('stopButton').style.display = 'none';
+}
+
+document.getElementById('startButton').addEventListener('click', toggleTimer);
+document.getElementById('pauseButton').addEventListener('click', toggleTimer);
+document.getElementById('stopButton').addEventListener('click', stopTimer);
 updateTimerDisplay();
